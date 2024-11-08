b857d2d813c36b5f9bcd150f1708b339a5d8b19b
Stephan <Stephan@capitolssg.com>
2024-11-08 13:30:36 -0500

    integraiton JS

--- resources/views/integration.blade.php
+++ resources/views/integration.blade.php
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("LOLLOL Integration Loaded...");
+    const init = () => {
+        const button = document.createElement('button');
+        button.textContent = 'lollol';
+        document.body.appendChild(button);
+        button.style.cssText = "position:absolute; bottom:2rem; left: 2rem; width: 80px; height: 80px; background-color:white; color: #ef4444; border-radius:50%; font-size:1.5rem; box-shadow: rgba(0, 0, 0, 0.3) 0px 2px 6px;";
+        button.addEventListener('click', () => {
+            alert('lollol');
+        });
+    };
+
+    init();
+});
+";
+
+    }();
 })
